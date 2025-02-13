3c825a156d863e51c8e3e5be9a2ef5e1310decb0
Genlocker <ogunrukuseun@gmail.com>
2025-02-13 10:32:54 +0100

    redirect to /login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </div>
-  );
+  const router = useRouter();
+
+  // Redirect to /login immediately when the component mounts
+  router.push("/login");
+
+  return null; // Optional: You can return null since the user will be redirected
 }
